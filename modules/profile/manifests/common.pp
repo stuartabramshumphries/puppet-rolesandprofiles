048899b2ea9ae8aadf4bb5a2be65b3759b2dbd69
Stuart Abrams-Humphries <stuartabramshumphries@gmail.com>
2017-12-03 18:23:28 +0000

    added git-cola

--- modules/profile/manifests/common.pp
+++ modules/profile/manifests/common.pp
@@ -1,5 +1,5 @@
 class profile::common {
-  $extras=['tmux','git','strace','htop']
+  $extras=['tmux','git','strace','htop','git-cola']
   package { $extras: ensure => 'installed' }
   include ntp
 }
